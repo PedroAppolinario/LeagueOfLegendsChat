1d6fb236d5b42b6a7cc8351e77b88280185e9315
Pedro B. Appolinario <pedrobarcellosappolinario@gmail.com>
2024-03-25 23:31:29 -0300

    Primeiro commit: Spring

---
+++ src/main/java/chat/leagueoflegends/loliachat/LeagueOfLegendsIaChatApplication.java
@@ -0,0 +1,13 @@
+package chat.leagueoflegends.loliachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOfLegendsIaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueOfLegendsIaChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chat/leagueoflegends/loliachat/LeagueOfLegendsIaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package chat.leagueoflegends.loliachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueOfLegendsIaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
